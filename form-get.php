374111e2c33d39269f4e0cf809663b307d9dd905
botezalexandru <botez14alexandru@gmail.com>
2015-08-28 12:06:39 +0300

    indent code

--- form-get.php
+++ form-get.php
@@ -2,28 +2,24 @@
 
             if(isset($_POST['submit'])) {
 
-
             $name = $_POST['userName'];
             $visitor_email = $_POST['email'];
             $email_subject = $_POST['subject'];
             $project_body = $_POST['project-description'];
 
+            require_once('../../../wp-config.php');
 
- 
-          
-          require_once('../../../wp-config.php');
+             global $wpdb;
+             $table_name = 'form_entries';
 
-                    global $wpdb;
-                    $table_name = 'form_entries';
-
-                    $wpdb->insert( 
+            $wpdb->insert( 
                         $table_name, 
                         array(
                             "name" => $name,
                             "email" => $visitor_email,
                             "subject" => $email_subject,
                             "projectdescription" => $project_body ),
-                            array('%s','%s','%s','%s'));
+                        array('%s','%s','%s','%s'));
 
             }
             ?>
